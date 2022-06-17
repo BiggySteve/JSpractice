a7f8fb5696875196c29fbba5d27ada2460ee2f8b
Steve Bransford <bransfordsteve@gmail.com>
2022-06-17 15:30:49 -0600

    changes to Cola practice with Head First book

--- cola.js
+++ cola.js
@@ -1,4 +1,4 @@
-var products = [ { name: "Grapefruit", calories: 170, color: "red", sold: 8200 },
+let products = [ { name: "Grapefruit", calories: 170, color: "red", sold: 8200 },
                  { name: "Orange", calories: 160, color: "orange", sold: 12101 },
                  { name: "Cola", calories: 210, color: "caramel", sold: 25412 },
                  { name: "Diet Cola", calories: 0, color: "caramel", sold: 43922 },
@@ -7,6 +7,36 @@ var products = [ { name: "Grapefruit", calories: 170, color: "red", sold: 8200 }
                  { name: "Root Beer", calories: 200, color: "caramel", sold: 9909 },
                  { name: "Water", calories: 0, color: "clear", sold: 62123 },
                ];
+
+function compareName(colaA, colaB) {
+    if (colaA.name > colaB.name) {
+        return 1;
+    } else if (colaA.name === colaB.name) {
+        return 0
+    } else {
+        return -1;
+    }
+}
+
+function compareCalories(colaA, colaB) {
+    if (colaA.calories > colaB.calories) {
+        return 1;
+    } else if (colaA.calories === colaB.calories) {
+        return 0
+    } else {
+        return -1;
+    }
+}
+
+function compareColor(colaA, colaB) {
+    if (colaA.color > colaB.color) {
+        return 1;
+    } else if (colaA.color === colaB.color) {
+        return 0
+    } else {
+        return -1;
+    }
+}
 
 function compareSold(colaA, colaB) {
     if (colaA.sold > colaB.sold) {
@@ -27,6 +57,19 @@ function printProducts(products) {
     }
 }
 
+products.sort(compareName);
+console.log("Products sorted by name:");
+printProducts(products);
+
+products.sort(compareCalories);
+console.log("Products sorted by calories:");
+printProducts(products);
+
+products.sort(compareColor);
+console.log("Products sorted by color:");
+printProducts(products);
+
 products.sort(compareSold);
+console.log("Products sorted by sold:");
 printProducts(products);
 
